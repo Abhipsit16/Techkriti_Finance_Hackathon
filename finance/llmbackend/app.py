08c2b0364262e5cc1d4c447816e0e0bc8c6cf78e
Abhipsit <renubajpai9@gmail.com>
2025-03-28 08:50:13 +0530

    Improved the AI module

--- finance/llmbackend/app.py
+++ finance/llmbackend/app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain_experimental.agents import create_csv_agent, create_pandas_dataframe_agent
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -21,28 +21,26 @@ for table in soup.select('table.data-table'):
 combined_df = pd.concat(financial_tables)
 
 combined_df=combined_df.drop_duplicates()
-combined_df=combined_df.T
-combined_df = combined_df.set_index('Unnamed: 0').reset_index()
 
-
-
-# Clean and transpose data
-# data = pd.read_csv("screener_financial_data.csv").set_index('Unnamed: 0').T.reset_index()
-# data.columns = data.iloc[0]
-# data = data.drop(0).rename(columns={'index': 'Time'}).fillna(0).reset_index(drop=True)
-# data.to_csv("screener_financial_data.csv", index=False)
 
 # Initialize agents
 load_dotenv()
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", api_key=os.getenv('GEMINI_KEY'))
+llm = ChatGroq(groq_api_key = os.getenv('GROQ_API_KEY'),
+                model_name = "llama-3.1-8b-instant",
+                temperature = 0,
+               max_tokens = 6000)
+
 csv_agent = create_pandas_dataframe_agent(llm=llm, verbose=True, allow_dangerous_code=True, df=combined_df)
 
 # Analysis prompt
 analysis_template = """Analyze this financial data:
 - Identify trends in Sales, Expenses, and Profits
+- The order of your working will be as follows :
+1. If there are any anomalies in any row, just remove those rows
+2. Now, replace the null values of the left columns using The fastest method possible.
+3. Calculate the important features which are not in the dataframe, but the dataframe contain enough resources so, that you can make them.
+You need to STRICTLY follow the above given order one by one.
 - Ignore all the worthless columns like Raw PDF and duplicate columns
-- If you get anything in non numeric format or null, just remove those columns
-- Calculate YOY growth rates
 - Highlight all the key insights for investors
 - Use simple language avoiding technical terms"""
 
@@ -59,7 +57,7 @@ Example:
 plt.plot(data['Time'], data['Sales'])
 plt.savefig(os.path.join({current_directory}, 'sales_trend.png'))"""
 
-plot_result = csv_agent.invoke(plot_template.format(current_directory="C:/Users/abhip/Desktop/Techy/Techkriti_Finance_Hackathon/finance/public/plots"))
+plot_result = csv_agent.invoke(plot_template.format(current_directory='C:/Users/abhip/Desktop/Techy/Techkriti_Finance_Hackathon/finance/public/plots'))
 
 print("Analysis:", analysis_result['output'])
 print("Plots saved at:", plot_result['output'])
